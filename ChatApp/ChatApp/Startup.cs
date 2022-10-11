27aec6f05fadaf170d3dc5033a1d0493250876b6
Miguel Duarte <miguelsantoscoelhoduarte@gmail.com>
2022-10-11 19:58:12 +0100

    Changes

---
+++ ChatApp/ChatApp/Startup.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+
+namespace ChatApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddCors();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseRewriter(new RewriteOptions().AddIISUrlRewrite(env.WebRootFileProvider, "/files/IISUrlRewriteRules.xml"));
+
+            app.UseStaticFiles();
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.Run(context => context.Response.WriteAsync(
+            $"Rewritten or Redirected Url: " +
+            $"{context.Request.Path + context.Request.QueryString}"));
+        }
+    }
+}
---
+++ ChatApp/ChatApp/wwwroot/js/Message.js
@@ -0,0 +1,61 @@
+﻿
+function BuildMessage() {
+    for (let i = 0; i < 5; i++) {
+        let ol = document.createElement('ol');
+        ol.className = 'list-group';
+
+        let li = document.createElement('li');
+        li.className = 'list-group-item d-flex justify-content-between align-items-center';
+
+        let img = document.createElement('img');
+        img.setAttribute('src', 'https://www.w3schools.com/bootstrap5/img_avatar1.png');
+        img.setAttribute('alt', 'Logo');
+        img.setAttribute('width', '40');
+        img.className = 'rounded-pill';
+
+        let div = document.createElement('div');
+        div.className = 'ms-2 me-auto';
+        div.textContent = 'Cras justo odio';
+
+        let div2 = document.createElement('div');
+        div2.className = 'fw-bold';
+        div2.textContent = 'Subheading';
+
+        let span = document.createElement('span');
+        span.className = 'badge bg-primary rounded-pill';
+        span.textContent = '14';
+
+        ol.appendChild(li);
+        li.appendChild(img);
+        li.appendChild(div);
+        div.appendChild(div2);
+        li.appendChild(span);
+        document.body.appendChild(ol);
+    }
+}
+
+function BuildMessageSent() {
+
+}
+
+function BuildMessageReceived() {
+
+}
+
+function BuildMessagesHistory() {
+    for (let i = 0; i < ; i++) {
+        if (isMessageSent()) {
+            BuildMessageSent();
+        }
+        else if (isMessageReceived()) {
+            BuildMessageReceived();
+        }
+        if (IsEndOfMessageType()) {
+            BuildMessageSentTime();
+        }
+    }
+}
+
+window.onload = function () {
+    //BuildMessage();
+};
